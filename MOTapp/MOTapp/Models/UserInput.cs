4fcfb9e9982909038307177477fa66609065ed52
Tim <ryalin1286@gmail.com>
2024-04-08 19:15:42 +0100

    Add validation.

--- MOTapp/MOTapp/Models/UserInput.cs
+++ MOTapp/MOTapp/Models/UserInput.cs
@@ -1,7 +1,11 @@
-﻿namespace MotApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MotApp.Models
 {
     public class UserInput
     {
-        public string? registrationNumber { get; set; }
+        [Required]
+        [RegularExpression(@"^([A-Z]{3}\s?(\d{3}|\d{2}|d{1})\s?[A-Z])|([A-Z]\s?(\d{3}|\d{2}|\d{1})\s?[A-Z]{3})|(([A-HK-PRSVWY][A-HJ-PR-Y])\s?([0][2-9]|[1-9][0-9])\s?[A-HJ-PR-Z]{3})$", ErrorMessage = "Invalid UK Vehicle Registration Number entered.")]
+        public required string registrationNumber { get; set; }
     }
 }
